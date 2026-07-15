f99f10cd79be6ce70f29407a21769f8b3b93d2cd
birdtravel <birdtrvl@gmail.com>
2026-07-15 16:37:34 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Bird Travel Website Loaded");
-
-    const waButton = document.querySelector(".button");
-
-    if (waButton) {
-        waButton.addEventListener("mouseover", function () {
-            waButton.style.transform = "scale(1.05)";
-        });
-
-        waButton.addEventListener("mouseout", function () {
-            waButton.style.transform = "scale(1)";
-        });
-    }
-});
+console.log("Bird Travel loaded");
